fc111036c76626a139ed11b7cc50f43f58f3101b
austinv11 <austinv11@gmail.com>
2018-09-03 23:30:37 -0400

    Initial commit

---
+++ pypeline/_db.py
@@ -0,0 +1,16 @@
+from contextlib import contextmanager
+
+import plyvel
+
+
+@contextmanager
+def open_db(loc: str) -> plyvel.DB:
+    db = plyvel.DB(loc, create_if_missing=True)
+    yield db
+    db.close()
+
+
+@contextmanager
+def open_prefixed_db(loc: str, prefix: str) -> plyvel.DB:
+    with open_db(loc) as db:
+        yield db.prefixed_db(prefix.encode())
---
+++ pypeline/pypeline.py
@@ -0,0 +1,50 @@
+import ujson
+import xxhash
+from abc import ABC, abstractmethod
+
+from ._db import *
+
+
+def build_uid(self: "SerializableAction", *args, **kwargs) -> bytes:
+    x = xxhash.xxh64()
+    x.update(str(self.task_name).encode())
+    for arg in args:
+        x.update(str(arg).encode())
+    for k, v in kwargs.items():
+        x.update((str(k) + str(v)).encode())
+    return x.digest()
+
+
+async def serialize(obj: object) -> bytes:
+    return ujson.encode(obj, ensure_ascii=False).encode()
+
+
+async def deserialize(value: bytes) -> object:
+    return ujson.decode(value.decode())
+
+
+class SerializableAction(ABC):
+
+    def __init__(self, db_dir: str):
+        self.db_dir = db_dir
+
+    @abstractmethod
+    @property
+    def task_name(self) -> str: ...
+
+    @abstractmethod
+    async def execute(self, *args, **kwargs) -> object: ...
+
+    async def run(self, *args, **kwargs) -> object:
+        uid = build_uid(*args, **kwargs)
+        with open_prefixed_db(self.db_dir, uid) as db:
+            ret_val = db.get("_".join([self.task_name, uid]))
+
+        if ret_val is None:
+            ret_val = await self.execute(*args, **kwargs)
+            serialized = await serialize(ret_val)
+            with open_prefixed_db(self.db_dir, uid) as db:
+                db.put("_".join([self.task_name, uid]), serialized)
+            return ret_val
+
+        return await deserialize(ret_val)
