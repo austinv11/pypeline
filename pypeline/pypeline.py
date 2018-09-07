3c88a27e2d47b1f1163ceb1879c6f34f0b755da7
austinv11 <austinv11@gmail.com>
2018-09-07 16:12:47 -0400

    Create a process forking executor

--- pypeline/pypeline.py
+++ pypeline/pypeline.py
@@ -1,6 +1,8 @@
+import asyncio
 import time
-from collections import namedtuple
-from typing import Tuple, List
+from collections import namedtuple, OrderedDict
+from concurrent.futures import ThreadPoolExecutor
+from typing import List
 
 import xxhash
 from abc import ABC, abstractmethod
@@ -75,7 +77,7 @@ class SimplePypelineExecutor(PypelineExecutor):
     considered deterministic.
     """
 
-    async def run(self, pypeline: 'Pypeline') -> List[ResultsHolder]:
+    async def run(self, pypeline: 'Pypeline'):
         curr_args = None
         for step in pypeline.steps:
             if not curr_args:
@@ -86,7 +88,58 @@ class SimplePypelineExecutor(PypelineExecutor):
                     new_args += await step.run(*arg_set.args, **arg_set.kwargs)
                 curr_args = new_args
 
-        return curr_args
+
+class ForkingPypelineExecutor(PypelineExecutor):
+
+    def __init__(self, max_forking_factor=4):
+        self.max_forking_factor = max_forking_factor
+
+    async def run(self, pypeline: 'Pypeline'):
+        linked_map = OrderedDict()
+        # Convert to a linked(ish) list
+        for i, v in enumerate(pypeline.steps):
+            if i == len(pypeline.steps) - 1:
+                linked_map[v] = None
+            else:
+                linked_map[v] = pypeline.steps[i+1]
+
+        curr_step = pypeline.steps[0]
+        await self.continue_run(linked_map, curr_step)
+
+    async def continue_run(self, linked_map, curr_step, *args, **kwargs):
+        results = await curr_step.run(*args, **kwargs)
+        forking_factor = min(self.max_forking_factor, len(results))
+        curr_step = linked_map[curr_step]
+
+        if curr_step is None or forking_factor == 0:
+            return
+
+        if forking_factor == 1:
+            def submit_task(coroutine):
+                return asyncio.ensure_future(coroutine, loop=asyncio.get_event_loop())
+        else:
+            pool = ThreadPoolExecutor(max_workers=forking_factor, thread_name_prefix="PypelineExecutor")
+            def submit_task(coroutine):
+                return asyncio.get_event_loop().run_in_executor(pool, coroutine)
+
+        for result in results:
+            coro = self._noarg_coro_builder(curr_step.run, *result.args, **result.kwargs)
+
+            future = submit_task(coro)
+            future.add_done_callback(self._callback_builder(curr_step, linked_map))
+
+    def _noarg_coro_builder(self, coro, *args, **kwargs):
+        async def coro1(): await coro(*args, **kwargs)
+        return coro1
+
+    def _callback_builder(self, next_step, linked_map):
+        def callback(future):
+            next = next_step
+            results = future.result()
+            for res in results:
+                asyncio.ensure_future(self.continue_run(linked_map, next, *res.args, **res.kwargs))
+
+        return callback
 
 
 class Pypeline:
